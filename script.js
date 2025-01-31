cce3a87c78e59889a1d6638aa00e1f04bb4bfdb2
Tulio050408 <jcesarhz05@gmail.com>
2025-01-31 00:51:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -27,9 +27,9 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "yeiii";
   buttonsContainer.classList.add("hidden");
-  changeImage("yes");
+  changeImage("sii");
 }
 
 function resizeYesButton() {
@@ -43,11 +43,11 @@ function resizeYesButton() {
 function generateMessage(noCount) {
   const messages = [
     "No",
-    "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
-    "I'm gonna cry...",
+    "seguraa?",
+    "Enserio?",
+    "mmya",
+    "esta bien :(",
+    "pipi",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
